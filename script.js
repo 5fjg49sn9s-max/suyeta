f9102a872e16a512e7dda2e6af279afaeee5ae33
5fjg49sn9s-max <5fjg49sn9s@privaterelay.appleid.com>
2026-09-22 19:13:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function otkrytOkno() {
+    // Просто красиво открываем окно с колой прямо на сайте
+    document.getElementById('secretModal').style.display = 'flex';
+}
+
+function zakrytOkno() {
+    // Закрываем окно, когда нажали кнопку
+    document.getElementById('secretModal').style.display = 'none';
+}
